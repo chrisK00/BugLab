d93d630e032a6b550463fed6eb952506371ff10b
chrisK00 <christian.khalil2000@gmail.com>
2021-11-05 12:07:04 +0100

    added serilog

--- BugLab.API/Program.cs
+++ BugLab.API/Program.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace BugLab.API
 {
@@ -7,14 +10,24 @@ namespace BugLab.API
     {
         public static void Main(string[] args)
         {
+            ConfigureLogger();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        public static void ConfigureLogger()
+        {
+            Log.Logger = new LoggerConfiguration()
+           .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+           .WriteTo.Console(theme: AnsiConsoleTheme.Code)
+           .CreateLogger();
+        }
     }
 }
