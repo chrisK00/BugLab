61ee9b36474479b96c384484f453b7f5f68a8fb8
chrisK00 <christian.khalil2000@gmail.com>
2021-11-07 13:43:49 +0100

    added blazor app

---
+++ BugLab.Blazor/Helpers/Endpoints.cs
@@ -0,0 +1,8 @@
+﻿namespace BugLab.Blazor.Helpers
+{
+    public static class Endpoints
+    {
+        public const string Projects = "/projects";
+        public const string Bugs = "/bugs";
+    }
+}
---
+++ BugLab.Blazor/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using MudBlazor.Services;
+using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace BugLab.Blazor
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddScoped<IRestClient>(x => new RestClient(new Uri($"{builder.HostEnvironment.BaseAddress}api")));
+
+            builder.Services.AddMudServices().AddMudBlazorSnackbar(cfg =>
+            {
+                cfg.ClearAfterNavigation = false;
+                cfg.ShowCloseIcon = true;
+                cfg.PreventDuplicates = false;
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
