0a545ee80dc58f6a6ba0dbe209c57f6894bb371b
chrisK00 <christian.khalil2000@gmail.com>
2021-11-07 17:24:45 +0100

    querybuilder

---
+++ BugLab.Shared/Helpers/HttpClientHelpers/QueryBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace BugLab.Shared.Helpers.HttpClientHelpers
+{
+    public class QueryBuilder : IKeyValueBuilder
+    {
+        private readonly StringBuilder _sb;
+
+        protected QueryBuilder(string uri)
+        {
+            _sb = new StringBuilder(uri);
+        }
+
+        public IKeyValueBuilder WithParam(string key, string value)
+        {
+            Append(key, value);
+            return this;
+        }
+
+        public IKeyValueBuilder WithParam<T>(string key, T value)
+        {
+            Append(key, $"{value}");
+            return this;
+        }
+
+        private void Append(string key, string value)
+        {
+            if (_sb[^1] != '?') _sb.Append('&');
+
+            _sb.Append(key)
+                .Append('=')
+                .Append(Uri.EscapeDataString(value));
+        }
+
+        public static IKeyValueBuilder Use(string uri)
+        {
+            return new QueryBuilder(uri.EndsWith('?') ? uri : $"{uri}?");
+        }
+
+        public string Build()
+        {
+            return _sb.ToString();
+        }
+    }
+}
---
+++ BugLab.Tests/Shared/QueryBuilderTests.cs
@@ -0,0 +1,25 @@
+﻿using BugLab.Shared.Helpers.HttpClientHelpers;
+using FluentAssertions;
+using Xunit;
+
+namespace BugLab.Tests.Shared
+{
+    public class QueryBuilderTests
+    {
+        [Fact]
+        public void WithParam_EscapesValues_WithPercentageSign_In_Sentence()
+        {
+            var hello = "hello";
+            var world = "world";
+
+            var result = QueryBuilder.Use("api")
+                .WithParam("sentence", $"{hello} {world}")
+                .Build();
+
+            result.Should().Contain("%");
+            var splittedResult = result.Split('%');
+            splittedResult[^1].Should().ContainAll(world, "20");
+            splittedResult[^2].Should().Contain(hello);
+        }
+    }
+}
