63e158c39c3acda853a778d5b20da750b10709a1
chrisK00 <christian.khalil2000@gmail.com>
2021-11-08 14:07:20 +0100

    unit tests

---
+++ BugLab.Tests/Business/QueryHandlers/GetProjectHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using BugLab.Business.QueryHandlers.Projects;
+using BugLab.Shared.Queries;
+using BugLab.Tests.Helpers;
+using FluentAssertions;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BugLab.Tests.Business.QueryHandlers
+{
+    public class GetProjectHandlerTests
+    {
+        private GetProjectHandler _sut;
+
+        [Fact]
+        public async Task GetProject_ReturnsNull_WhenNotFound()
+        {
+            using var context = DbContextHelpers.Create();
+            _sut = new(context);
+            var result = await _sut.Handle(new GetProjectQuery(default), default);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public async Task GetProject_ReturnsSpecifiedProject_With_BugsCount()
+        {
+            using var context = DbContextHelpers.Create();
+            _sut = new(context);
+            var projectId = context.Projects.FirstOrDefault().Id;
+            var result = await _sut.Handle(new GetProjectQuery(projectId), default);
+
+            result.Should().NotBeNull();
+            result.Id.Should().Be(projectId);
+            result.TotalBugs.Should().BeGreaterThan(0);
+            result.TotalHighPriorityBugs.Should().BeGreaterThan(0);
+        }
+    }
+}
---
+++ BugLab.Tests/Helpers/DbContextHelpers.cs
@@ -0,0 +1,44 @@
+﻿using BugLab.Data;
+using BugLab.Data.Entities;
+using BugLab.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using TestSupport.EfHelpers;
+
+namespace BugLab.Tests.Helpers
+{
+    public static class DbContextHelpers
+    {
+        public static AppDbContext Create()
+        {
+            var options = SqliteInMemory.CreateOptions<AppDbContext>();
+            var context = new AppDbContext(options);
+            context.Database.EnsureCreated();
+            context.SeedProjects();
+            context.SeedBugs();
+            context.SaveChanges();
+
+            return context;
+        }
+
+        public static void SeedProjects(this AppDbContext context)
+        {
+            context.Projects.AddRange(new List<Project>
+            {
+                new Project { Title = "project1"},
+                new Project { Title = "project2"},
+                new Project { Title = "project3"},
+            });
+        }
+
+        public static void SeedBugs(this AppDbContext context)
+        {
+            context.Bugs.AddRange(new List<Bug>
+            {
+                new Bug { Title = "bug1", Priority = BugPriority.High, Status = BugStatus.Open, ProjectId = 1 },
+                new Bug { Title = "bug2", ProjectId = 1 },
+                new Bug { Title = "bug3", ProjectId = 2 },
+            });
+        }
+    }
+}
