662f4da698fd641939bcf5a78e2d87abb44605b0
chrisK00 <christian.khalil2000@gmail.com>
2021-11-08 20:50:35 +0100

    bugslist and simple card

---
+++ BugLab.Blazor/Helpers/JsonOptions.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace BugLab.Blazor.Helpers
+{
+    public static class JsonOptions
+    {
+        public static JsonSerializerOptions Defaults()
+        {
+            var options = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+            options.Converters.Add(new JsonStringEnumConverter());
+            return options;
+        }
+    }
+}
