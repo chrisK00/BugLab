62fc5cb95f94b760d53914875fc7d855a8ffcce9
chrisK00 <christian.khalil2000@gmail.com>
2021-11-10 15:06:59 +0100

    add bug blazor

--- BugLab.API/Controllers/BugsController.cs
+++ BugLab.API/Controllers/BugsController.cs
@@ -4,7 +4,6 @@ using BugLab.Shared.Queries;
 using BugLab.Shared.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
