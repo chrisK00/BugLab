7ae542b479f9c639e18f59b6c831b55ba8f27fff
chrisK00 <christian.khalil2000@gmail.com>
2021-11-21 10:30:31 +0100

    delete bug and bugtype handler tests

---
+++ BugLab.Tests/Business/CommandHandlers/DeleteBugHandlerTests.cs
@@ -0,0 +1,30 @@
+﻿using BugLab.Business.CommandHandlers.Bugs;
+using BugLab.Business.Commands.Bugs;
+using BugLab.Tests.Helpers;
+using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BugLab.Tests.Business.CommandHandlers
+{
+    public class DeleteBugHandlerTests
+    {
+        private DeleteBugHandler _sut;
+        private DeleteBugCommand _command = new(1);
+
+        [Fact]
+        public async Task SoftDeletes()
+        {
+            using var context = await DbContextHelpers.CreateAsync();
+            _sut = new(context);
+            await _sut.Handle(_command, default);
+
+            var deletedBug = context.Bugs.IgnoreQueryFilters().First(x => x.Id == _command.Id);
+            deletedBug.Should().NotBeNull();
+            deletedBug.Deleted.Should().BeCloseTo(DateTime.UtcNow, TimeSpan.FromMinutes(5));
+        }
+    }
+}
---
+++ BugLab.Tests/Business/CommandHandlers/DeleteBugTypeHandlerTests.cs
@@ -0,0 +1,39 @@
+﻿using BugLab.Business.CommandHandlers.BugTypes;
+using BugLab.Business.Commands.BugTypes;
+using BugLab.Tests.Helpers;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BugLab.Tests.Business.CommandHandlers
+{
+    public class DeleteBugTypeHandlerTests
+    {
+        private DeleteBugTypeHandler _sut;
+        private DeleteBugTypeCommand _command;
+
+        [Fact]
+        public async Task Throws_WhenBugType_IsNotFound()
+        {
+            using var context = await DbContextHelpers.CreateAsync();
+            _sut = new(context);
+            _command = new(int.MaxValue);
+
+            await _sut.Invoking(_ => _.Handle(_command, default))
+                .Should().ThrowAsync<KeyNotFoundException>();
+        }
+
+        [Fact]
+        public async Task Throws_WhenBugType_IsUsedByOtherBugs()
+        {
+            using var context = await DbContextHelpers.CreateAsync();
+            _sut = new(context);
+            _command = new(1);
+
+            await _sut.Invoking(_ => _.Handle(_command, default))
+                .Should().ThrowAsync<InvalidOperationException>();
+        }
+    }
+}
