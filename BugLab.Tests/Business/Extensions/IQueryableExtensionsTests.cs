35f79ca6c593007fda209682b3f25416593f87c4
chrisK00 <christian.khalil2000@gmail.com>
2021-11-21 10:38:59 +0100

    paginateasync test and remove old todo comments

---
+++ BugLab.Tests/Business/Extensions/IQueryableExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using BugLab.Business.Extensions;
+using BugLab.Tests.Helpers;
+using FluentAssertions;
+using Microsoft.Azure.Cosmos.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BugLab.Tests.Business.Extensions
+{
+    public class IQueryableExtensionsTests
+    {
+        [Fact]
+        public async Task PaginateAsync_Returns_AmountOfTotalItems()
+        {
+            var context = await DbContextHelpers.CreateAsync();
+            var expectedBugCount = await context.Bugs.CountAsync();
+
+            var (query, bugCount) = await context.Bugs.PaginateAsync(1, 1, default);
+
+            bugCount.Should().Be(expectedBugCount);
+        }
+    }
+}
